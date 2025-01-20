634bc0ec503de1c0cef3e269cbe1dfb5eafae422
Daniel Babalola <dannybabs123456@gmail.com>
2025-01-19 20:52:53 -0500

    feat: Add Property model and provider; implement property loading and filtering functionality,

---
+++ lib/features/profile/data/repositories/property.dart
@@ -0,0 +1,27 @@
+import 'dart:convert';
+import 'package:flutter/services.dart';
+
+import '../models/property.dart';
+
+class PropertyProvider {
+  static Future<List<Property>> loadProperties() async {
+    try {
+      final String response = await rootBundle
+          .loadString('lib/features/home/data/datasources/response.json');
+      final data = json.decode(response);
+
+      return (data['properties'] as List)
+          .map((json) => Property.fromJson(json))
+          .toList();
+    } catch (e) {
+      // Handle errors appropriately
+      print('Error loading properties: $e');
+      return [];
+    }
+  }
+
+  static List<Property> filterByStatus(
+      List<Property> properties, String status) {
+    return properties.where((property) => property.status == status).toList();
+  }
+}
