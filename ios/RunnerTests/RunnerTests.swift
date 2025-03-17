0c375d914cc49917ab3df7300af24375d99d9af2
Daniel Babalola <dannybabs123456@gmail.com>
2025-03-17 17:33:10 -0400

    refactor: Update iOS project settings and enhance AppDelegate with navigation bar appearance and deep linking support

--- ios/RunnerTests/RunnerTests.swift
+++ ios/RunnerTests/RunnerTests.swift
@@ -1,12 +1,37 @@
 import Flutter
 import UIKit
 import XCTest
+@testable import Runner
 
 class RunnerTests: XCTestCase {
-
-  func testExample() {
-    // If you add code to the Runner application, consider adding tests here.
-    // See https://developer.apple.com/documentation/xctest for more information about using XCTest.
+  
+  var appDelegate: AppDelegate!
+  
+  override func setUp() {
+    super.setUp()
+    appDelegate = AppDelegate()
   }
-
+  
+  override func tearDown() {
+    appDelegate = nil
+    super.tearDown()
+  }
+  
+  func testAppInitialization() {
+    // Test that the app delegate initializes properly
+    XCTAssertNotNil(appDelegate, "App delegate should not be nil")
+  }
+  
+  func testTabResetToHomeOnLaunch() {
+    // Test that tab is reset to home on fresh launch
+    let defaults = UserDefaults.standard
+    let lastTab = defaults.integer(forKey: "last_selected_tab")
+    XCTAssertEqual(lastTab, 0, "App should reset to home tab (index 0) on launch")
+  }
+  
+  func testNavigationBarConfiguration() {
+    // This is just a placeholder test for now
+    // In a real test, you would verify navigation bar appearance
+    XCTAssertTrue(true, "Navigation bar should be properly configured")
+  }
 }
